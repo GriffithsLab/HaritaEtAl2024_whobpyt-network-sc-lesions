f6f0e0918a00a6c2035099266b4ded29d9733ca0
Shreyas Harita <50175632+Shreyder93@users.noreply.github.com>
2024-06-25 13:06:08 -0400

    Update and rename WhoBPyT_HCP_subs_version_4.py to WhoBPyT_HCP_subs_task_sims.py

--- Code/WhoBPyT_HCP_subs_task_sims.py
+++ Code/WhoBPyT_HCP_subs_task_sims.py
@@ -1,14 +1,7 @@
-# This is version 3.
-# Here I basically use the same script as WhoBPyT_HCP_subs_version_2.py but this is for task fMRI.
+# For task fMRI.
 
-# Here I combine all the tasks using a second user input which is one of : 'WM', 'MOTOR', 'SOCIAL', or 'LANGUAGE' for the different tasks
-
-# ------------------------------------
-
-# IMPORTANT!
-
-# TR = 720 ms or 0.72 (not 2 sec!)
-
+# Here I combine all the tasks using a second user input which is one of: 'WM', 'MOTOR', or 'LANGUAGE' for the different tasks
+# Dropped Tasks: 'SOCIAL', 'GAMBLING'
 # ------------------------------------
 
 # Resolution: 200 Schaefer Parcellations (possible with higher).
@@ -32,7 +25,6 @@ import seaborn as sns
 # whobpyt stuff
 import whobpyt
 from whobpyt.data.dataload import dataloader
-# from whobpyt.models.jansen_rit import RNNJANSEN
 from whobpyt.models.wong_wang import RNNRWW
 from whobpyt.datatypes.modelparameters import ParamsModel
 from whobpyt.optimization.modelfitting import Model_fitting
@@ -72,7 +64,7 @@ print("Stuff setup ...")
 # Paths
 
 Wts_Path = '/external/rprshnas01/netdata_kcni/jglab/MemberSpaces/Data/Shrey/Improved_WWD_HCP_model_runs/All_Subs_SC_Wts/Davide_HCP_Data_Matrix'
-
+# replace the above with your weights path!
 
 parcs = np.arange(0,200,1)
 
@@ -80,7 +72,7 @@ print("Paths set ...")
 
 # ----------------------------------------------------------------------------------------------------------------
 
-# define options for wong-wang model
+# define options for the wong-wang model
 node_size = 200
 mask = np.tril_indices(node_size, -1)
 num_epoches = 60 #40 #20 #50 
@@ -116,11 +108,10 @@ print("Loaded SC ...")
 # Load FC
 
 pconn_path = '/external/rprshnas01/netdata_kcni/jglab/MemberSpaces/Data/Shrey/Shrey_SS_parcellated_Func_Conns_IV/'
+# The above path contains parcellated empirical fMRI data. See the connectome workbench script for more information.  
 
 if task_choice == 'MOTOR':
     pconn1LR = pconn_path + '{0}_tfMRI_MOTOR_RL_200Schaefer_7Ntwx_cifti_parcellated.ptseries.nii'.format(sub_id)
-elif task_choice == 'SOCIAL':
-    pconn1LR = pconn_path + '{0}_tfMRI_SOCIAL_RL_200Schaefer_7Ntwx_cifti_parcellated.ptseries.nii'.format(sub_id)
 elif task_choice == 'WM':
     pconn1LR = pconn_path + '{0}_tfMRI_WM_RL_200Schaefer_7Ntwx_cifti_parcellated.ptseries.nii'.format(sub_id)
 elif task_choice == 'LANGUAGE':
@@ -150,15 +141,16 @@ data_mean = dataloader(ts, num_epoches, batch_size)
 par = ParamsModel('RWW',  g=[400, 1/np.sqrt(10)], g_EE=[1.5, 1/np.sqrt(50)], g_EI =[0.8,1/np.sqrt(50)], \
                           g_IE=[0.6,1/np.sqrt(50)], I_0 =[0.2, 0], I_external=[0.02,0], std_in=[0.0,0], std_out=[0.00,0])
 
-# note that I_external has been set to 0.02 in the above params as this is task sims. 
-# This param is 0 for rest sims. 
+# note that I_external has been set to 0.02 in the above params as this is task sims (see Deco et al., 2014). 
+# Quick observation (based on comparison with 1st 10 subjects): I_ext has *no significant* (p >> 0.05) effect on model fit to empirical task data. It is practically identical when I_ext = 0.  
+# I_ext = 0 for the rest sims. 
 
 print("Loaded other Stuff ...Running WhoBPyT... ")
 
 # ----------------------------------------------------------------------------------------------------------------
 
 # %%
-# call model want to fit
+# call model you want to fit
 model = RNNRWW(node_size, batch_size, step_size, repeat_size, tr, sc, True, par)
 
 # %%
@@ -183,26 +175,14 @@ print("Finished running WhoBPyT ...")
 
 # ----------------------------------------------------------------------------------------------------------------
 
-# output_path = '/external/rprshnas01/netdata_kcni/jglab/MemberSpaces/Data/Shrey/WhoBPyT/200_subjects_WhoBPyT_task_run_pkls'
+
 # output_path = '/external/rprshnas01/netdata_kcni/jglab/MemberSpaces/Data/Shrey/WhoBPyT/200_subjects_WhoBPyT_task_run_pkls_real_time'
-# output_path = '/external/rprshnas01/netdata_kcni/jglab/MemberSpaces/Data/Shrey/WhoBPyT/200_subjects_WhoBPyT_task_run_pkls_fake_time'
-
-output_path = '/external/rprshnas01/netdata_kcni/jglab/MemberSpaces/Data/Shrey/WhoBPyT/sample_10_subjects_WhoBPyT_task_run_pkls_real_time_w_Iext'
-
-# filename = output_path + '/Subj_{0}_fittingresults_motor_task_stim_exp.pkl'.format(sub_id)
-# filename = output_path + '/Subj_{0}_fittingresults_social_task_stim_exp.pkl'.format(sub_id)
-# filename = output_path + '/Subj_{0}_fittingresults_wm_task_stim_exp.pkl'.format(sub_id)
-# filename = output_path + '/Subj_{0}_fittingresults_language_task_stim_exp.pkl'.format(sub_id)
 
 filename = output_path + '/Subj_{0}_fittingresults_{1}_task_stim_exp.pkl'.format(sub_id, task_choice)
 
 with open(filename, 'wb') as f:
     pickle.dump(F, f)
     
-# outfilename = output_path + '/Subj_{0}_simEEG_stim_exp.pkl'.format(sub_id)
-# with open(outfilename, 'wb') as f:
-#     pickle.dump(F.output_sim, f)
-
 print("Output saved!")
 
 # ----------------------------------------------------------------------------------------------------------------
